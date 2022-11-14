c49ad725bdc813e24a4d421f4502e45d3e09748e
“Khh98” <khh16@aub.edu.lb>
2022-11-14 20:43:40 +0200

    First

---
+++ pages/02_✔_Image_recommendation.py
@@ -0,0 +1,181 @@
+import streamlit as st
+from PIL import Image
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+import requests
+import plotly.express as px
+import numpy as np 
+import pandas as pd 
+import matplotlib
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import matplotlib.pyplot as plt
+import urllib.parse
+
+
+
+import tensorflow as tf
+import keras
+from keras import Model
+from tensorflow.keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.layers import GlobalMaxPooling2D
+from sklearn.metrics.pairwise import pairwise_distances
+
+import tensorflow as tf
+from keras.preprocessing import image
+from keras.layers import Input
+from keras.backend import reshape
+from keras.utils import load_img,img_to_array,array_to_img
+from sklearn.neighbors import NearestNeighbors
+import cv2
+
+import os
+
+import re
+
+#load listings data of 7 cities
+
+#load listings data of 7 cities
+
+listings2 = pd.read_csv("listings2.csv")
+
+
+
+# clean the price column
+conv_price = lambda x: str(x)[1:].replace(',', '') if '$' in str(x) else x
+
+listings2['price'] = listings2['price'].apply(conv_price)
+listings2['price'] = listings2['price'].apply(lambda x: float(x))
+
+
+
+
+
+def loadImages(path):
+    '''Put files into lists and return them as one list with all images 
+     in the folder'''
+    image_files = sorted([os.path.join(path, file)
+                          for file in os.listdir(path)])
+    return image_files
+
+
+
+path = r"C:\Users\karim\OneDrive\Desktop\exploreairbnb\images"
+image_paths=loadImages(path)
+
+#extract images from url column data to match it with the images loaded
+import urllib.parse
+
+urls = list(listings2['picture_url'])
+extract_images = [urllib.parse.urlparse(i)[2].rpartition('/')[2] for i in urls]
+
+#add column
+listings2['images'] = extract_images
+
+df2 = pd.read_csv("paths.csv")
+df2.drop("paths",axis=1,inplace=True)
+
+df2["path"] = image_paths
+
+images_data=df2.merge(listings2, how='left')
+images_data.to_csv("images.csv")
+#model
+
+def get_embedding(model, img_name):
+    # Reshape
+    img = load_img(img_name, target_size=(img_width, img_height))
+    # img to Array
+    x   = img_to_array(img)
+    # Expand Dim (1, w, h)
+    x   = np.expand_dims(x, axis=0)
+    # Pre process Input
+    x   = preprocess_input(x)
+    return model.predict(x).reshape(-1)
+
+
+
+
+
+# Pre-Trained Model
+# Input Shape
+img_width, img_height, _ = 224, 224, 3 
+
+base_model = ResNet50(weights='imagenet', 
+                      include_top=False, 
+                      input_shape = (img_width, img_height, 3))
+base_model.trainable = False
+
+# Add Layer Embedding
+model = keras.Sequential([
+    base_model,
+    GlobalMaxPooling2D()
+])
+
+df_embs = pd.read_csv("embeddings2.csv")
+# Calcule DIstance Matrix
+cosine_sim = 1-pairwise_distances(df_embs, metric='cosine')
+
+indices = pd.Series(range(len(images_data)), index=images_data.index)
+def get_recommender(idx, df, top_n = 5):
+    sim_idx    = indices[idx]
+    sim_scores = list(enumerate(cosine_sim[sim_idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:top_n+1]
+    idx_rec    = [i[0] for i in sim_scores]
+    idx_sim    = [i[1] for i in sim_scores]
+    
+    return indices.iloc[idx_rec].index, idx_sim
+
+
+
+def load_image(img, resized_fac = 0.1):
+    img     = cv2.imread(img)
+    w, h, _ = img.shape
+    resized = cv2.resize(img, (int(h*resized_fac), int(w*resized_fac)), interpolation = cv2.INTER_AREA)
+    return img
+
+
+def plot_figures(figures, nrows = 1, ncols=1,figsize=(8, 8)):
+    """Plot a dictionary of figures.
+
+    Parameters
+    ----------
+    figures : <title, figure> dictionary
+    ncols : number of columns of subplots wanted in the display
+    nrows : number of rows of subplots wanted in the figure
+    """
+
+    fig, axeslist = plt.subplots(ncols=ncols, nrows=nrows,figsize=figsize)
+    for ind,title in enumerate(figures):
+        axeslist.ravel()[ind].imshow(cv2.cvtColor(figures[title], cv2.COLOR_BGR2RGB))
+        axeslist.ravel()[ind].set_title(title)
+        axeslist.ravel()[ind].set_axis_off()
+    plt.tight_layout() # optional
+
+idx_ref = np.random.randint(1, images_data.shape[0], 1)[0]
+get_item = st.sidebar.button('Get Listing')
+        
+if get_item:
+            st.markdown(images_data["description"].loc[idx_ref],unsafe_allow_html=True)
+            st.markdown(images_data["listing_url"].loc[idx_ref],unsafe_allow_html=True)
+            # Recommendations
+            idx_rec, idx_sim = get_recommender(idx_ref, images_data, top_n = 6)
+
+            # Plot
+            #===================
+            plt.imshow(cv2.cvtColor(load_image(images_data.iloc[idx_ref].path), cv2.COLOR_BGR2RGB))
+            st.sidebar.image(cv2.cvtColor(load_image(images_data.iloc[idx_ref].path), cv2.COLOR_BGR2RGB), width=350)
+            st.sidebar.caption("Check the details of the following property here with similar listings👉")
+            # generation of a dictionary of (title, images)
+            figures = {str(i): load_image(row.path) for i, row in images_data.loc[idx_rec].iterrows()}
+            title = "<p style='text-align:left;color:DarkRed; font-size:20px;'>Compared to similar listings:</p>" 
+            st.markdown(title, unsafe_allow_html=True)
+            # plot of the images in a figure, with 3 rows and 2 columns
+            st.pyplot(plot_figures(figures, 3, 2))
+            title2 = "<p style='text-align:left;color:DarkRed; font-size:20px;'>For more info, vist us here:</p>" 
+            st.markdown(title2, unsafe_allow_html=True)
+            st.markdown(str(images_data["listing_url"].loc[idx_rec]),unsafe_allow_html=True)
---
+++ 🏠_Home.py
@@ -0,0 +1,238 @@
+import streamlit as st
+from PIL import Image
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+import requests
+import numpy as np 
+import pandas as pd 
+import matplotlib
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import matplotlib.pyplot as plt
+import urllib.parse
+
+
+
+import tensorflow as tf
+import keras
+from keras import Model
+from tensorflow.keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.layers import GlobalMaxPooling2D
+from sklearn.metrics.pairwise import pairwise_distances
+
+import tensorflow as tf
+from keras.preprocessing import image
+from keras.layers import Input
+from keras.backend import reshape
+from keras.utils import load_img,img_to_array,array_to_img
+import cv2
+
+import os
+
+import re
+
+import requests
+from streamlit_lottie import st_lottie
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# ---- LOAD ASSETS ----
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/private_files/lf30_5i5tlydx.json")
+
+
+
+#Homepage configuration
+
+im = Image.open("airbnb-2.jpg")
+
+st.set_page_config(
+    page_title="Airbnb",
+    page_icon= im,
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+hide_st_style = """
+<style>
+#MainMenu {visibility: hidden;}
+header{visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# ---Side-Bar----
+
+with st.sidebar:
+    st_lottie(lottie_coding, height=200, key="coding")
+    st.title("Analyzing users features for a smoother travel experience")
+    st.write('''
+    Hey 👋🏻there. My name is Karim Hazimeh and my teammate Mohammad Alalem, Zaka data science students. 
+    We enjoy working with data and extracting insightful information from them🎯.
+    ''')
+    st.write("---")
+
+
+
+#load listings data of 7 cities
+
+listings2 = pd.read_csv("listings2.csv")
+
+
+
+# clean the price column
+conv_price = lambda x: str(x)[1:].replace(',', '') if '$' in str(x) else x
+
+listings2['price'] = listings2['price'].apply(conv_price)
+listings2['price'] = listings2['price'].apply(lambda x: float(x))
+
+
+
+
+ #data exploration
+title3 = "<p style='text-align:center;color:red; font-size:30px;'>Data Exploration</p>" 
+st.markdown(title3, unsafe_allow_html=True)
+matplotlib.use("Agg")
+fig, ax = plt.subplots()
+matplotlib.rcParams.update({"font.size": 8})
+st.set_option("deprecation.showPyplotGlobalUse", False)
+
+
+def categorical_column(df, max_unique_values=15):
+    categorical_column_list = []
+    for column in df.columns:
+        if df[column].nunique() < max_unique_values:
+            categorical_column_list.append(column)
+    return categorical_column_list
+
+
+def eda(df):
+
+    # Show Dataset
+    if st.checkbox("Show Dataset"):
+        number = st.number_input("Numbers of rows to view", 5)
+        st.dataframe(df.head(number))
+
+    # Show Columns
+    if st.checkbox("Columns Names"):
+        st.write(df.columns)
+
+    # Show Shape
+    if st.checkbox("Shape of Dataset"):
+        st.write(df.shape)
+        data_dim = st.radio("Show Dimension by ", ("Rows", "Columns"))
+        if data_dim == "Columns":
+            st.text("Numbers of Columns")
+            st.write(df.shape[1])
+        elif data_dim == "Rows":
+            st.text("Numbers of Rows")
+            st.write(df.shape[0])
+        else:
+            st.write(df.shape)
+
+    # Select Columns
+    if st.checkbox("Select Column to show"):
+        all_columns = df.columns.tolist()
+        selected_columns = st.multiselect("Select Columns", all_columns)
+        new_df = df[selected_columns]
+        st.dataframe(new_df)
+
+    # Show Value Count
+    if st.checkbox("Show Value Counts"):
+        all_columns = df.columns.tolist()
+        selected_columns = st.selectbox("Select Column", all_columns)
+        st.write(df[selected_columns].value_counts())
+
+    # Show Datatypes
+    if st.checkbox("Show Data types"):
+        st.text("Data Types")
+        st.write(df.dtypes)
+
+    # Show Summary
+    if st.checkbox("Show Summary"):
+        st.text("Summary")
+        st.write(df.describe().T)
+
+    # Plot and visualization
+    st.subheader("Data Visualization")
+    all_columns_names = df.columns.tolist()
+
+    # Correlation Seaborn Plot
+    if st.checkbox("Show Correlation Plot"):
+        st.success("Generating Correlation Plot ...")
+        if st.checkbox("Annot the Plot"):
+            st.write(sns.heatmap(df.corr(), annot=True))
+        else:
+            st.write(sns.heatmap(df.corr()))
+        st.pyplot()
+
+    # Count Plot
+    if st.checkbox("Show Value Count Plots"):
+        x = st.selectbox("Select Categorical Column", all_columns_names)
+        st.success("Generating Plot ...")
+        if x:
+            if st.checkbox("Select Second Categorical column"):
+                hue_all_column_name = df[df.columns.difference([x])].columns
+                hue = st.selectbox("Select Column for Count Plot", hue_all_column_name)
+                st.write(sns.countplot(x=x, hue=hue, data=df, palette="Set2"))
+            else:
+                st.write(sns.countplot(x=x, data=df, palette="Set2"))
+            st.pyplot()
+
+    # Pie Chart
+    if st.checkbox("Show Pie Plot"):
+        all_columns = categorical_column(df)
+        selected_columns = st.selectbox("Select Column", all_columns)
+        if selected_columns:
+            st.success("Generating Pie Chart ...")
+            st.write(df[selected_columns].value_counts().plot.pie(autopct="%1.1f%%"))
+            st.pyplot()
+
+    # Customizable Plot
+    st.subheader("Customizable Plot")
+
+    type_of_plot = st.selectbox(
+        "Select type of Plot", ["area", "bar", "line", "hist", "box", "kde"]
+    )
+    selected_columns_names = st.multiselect("Select Columns to plot", all_columns_names)
+
+    if st.button("Generate Plot"):
+        st.success(
+            "Generating Customizable Plot of {} for {}".format(
+                type_of_plot, selected_columns_names
+            )
+        )
+
+        custom_data = df[selected_columns_names]
+        if type_of_plot == "area":
+            st.area_chart(custom_data)
+
+        elif type_of_plot == "bar":
+            st.bar_chart(custom_data)
+
+        elif type_of_plot == "line":
+            st.line_chart(custom_data)
+
+        elif type_of_plot:
+            custom_plot = df[selected_columns_names].plot(kind=type_of_plot)
+            st.write(custom_plot)
+            st.pyplot()
+unnecessary_columns= ['scrape_id', 'last_scraped', 'source', 'host_id','host_url','host_thumbnail_url','host_picture_url','host_neighbourhood','host_listings_count',
+'host_total_listings_count','host_verifications','neighbourhood_cleansed','neighbourhood_group_cleansed' ,'bathrooms',
+'minimum_minimum_nights','maximum_minimum_nights', 'minimum_maximum_nights','maximum_maximum_nights', 'minimum_nights_avg_ntm',
+'maximum_nights_avg_ntm', 'calendar_updated', 'calendar_last_scraped','number_of_reviews_ltm', 'number_of_reviews_l30d','license',
+'calculated_host_listings_count','calculated_host_listings_count_entire_homes','calculated_host_listings_count_private_rooms',
+'calculated_host_listings_count_shared_rooms', 'reviews_per_month','availability_30', 'availability_60', 'availability_90']
+explore = listings2.drop(unnecessary_columns,axis=1).dropna()
+eda(explore)
+
+
+    
